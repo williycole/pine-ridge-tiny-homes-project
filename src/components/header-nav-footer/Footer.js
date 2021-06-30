70081ff3dab5a11b907dade831d30fb1c578e77e
William Cole Boren <64283810+williycole@users.noreply.github.com>
2021-06-29 19:48:54 -0500

    Update Footer.js

--- src/components/header-nav-footer/Footer.js
+++ src/components/header-nav-footer/Footer.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Footer(){
         return (
             <footer>
-                 <ul>
-                    <li>Sponsor-1-image</li>
-                    <li>Sponsor-2-image</li>
-                    <li>Sponsor-3-image</li>
-                </ul>
+//                  <ul>
+//                     <li>Sponsor-1-image</li>
+//                     <li>Sponsor-2-image</li>
+//                     <li>Sponsor-3-image</li>
+//                 </ul>
             </footer>
         );
 }
