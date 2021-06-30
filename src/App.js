a4a063aed0bd8acb9ab69b027ed1209ddf66cb74
William Cole Boren <64283810+williycole@users.noreply.github.com>
2021-06-29 20:52:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Header from './components/header-nav-footer/Header'
 import Main from './components/Main';
-// import Footer from './components/header-nav-footer/Footer';
 
-//Change to functional component
 function App() {
     return (
       <div id="main-app-container">
         <Header />
         <Main />
-/*        <Footer /> */
       </div>
     );
 }
